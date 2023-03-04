7e9cbaf6376b5fa136f213dd7aa4c6d5f78068d8
wuxianggujun <3344207732@qq.com>
2023-03-04 18:12:27 +0800

    Initial commit

---
+++ tinasprout-core/src/main/java/com/wuxianggujun/tinasproutcore/TinaSproutCoreApplication.java
@@ -0,0 +1,13 @@
+package com.wuxianggujun.tinasproutcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinaSproutCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinaSproutCoreApplication.class, args);
+    }
+
+}
---
+++ tinasprout-core/src/test/java/com/wuxianggujun/tinasproutcore/TinaSproutCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuxianggujun.tinasproutcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinaSproutCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
