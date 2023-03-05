82b7c7b33ebfdbeacba1271ee2e4416d86d8090f
wuxianggujun <3344207732@qq.com>
2023-03-05 10:11:35 +0800

    添加新模块

---
+++ tinasprout-robot/src/test/java/com/wuxianggujun/tinasproutrobot/TinasproutRobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuxianggujun.tinasproutrobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinasproutRobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
